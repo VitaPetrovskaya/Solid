28f55aaa0e9bde840e3e7dd1472faebdbed13825
Vita Petrovskaya <vita.petrovskaya@gmail.com>
2019-02-01 16:19:25 +0300

    Add DataBase class

---
+++ SolidWorkshop/DataBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidWorkshop
+{
+    public class DataBase
+    {
+        private readonly string _connectionString;
+
+        protected readonly SqlConnection _sqlConnection;
+
+        public DataBase(string connectionString = "[connectionString]")
+        {
+            _connectionString = connectionString;
+            _sqlConnection = new SqlConnection(_connectionString);
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return _sqlConnection;
+            }
+        }
+    }
+}
