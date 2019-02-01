83a544b4012fc14b18a807ecf37b0e6fc984097e
Vita Petrovskaya <vita.petrovskaya@gmail.com>
2019-02-01 16:20:49 +0300

    Add interfaces

---
+++ SolidWorkshop/IReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidWorkshop
+{
+    public interface IReader
+    {
+        List<IEntity> ReadAll(DataBase database);
+    }
+}
---
+++ SolidWorkshop/ISaver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidWorkshop
+{
+    public interface ISaver
+    {
+        IEntity Save(DataBase database, IEntity entity);
+    }
+}
